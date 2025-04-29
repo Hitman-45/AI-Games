f89c32b4cc30a743d90d9dc576fae05e4919c8b5
Shankhesh <cs22b005@iittp.ac.in>
2025-04-29 23:49:04 +0530

    Running the Codes and Fine tuning it

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ import chess.svg
 import os
 
 def save_board_svg(board, move_number):
-    if not os.path.exists("frames"):
-        os.makedirs("frames")
+    if not os.path.exists("frames_min_max"):
+        os.makedirs("frames_min_max")
     svg = chess.svg.board(board=board, size=500)
     filepath = f"frames_min_max/board_{move_number:03d}.svg"
     with open(filepath, "w", encoding="utf-8") as f:
